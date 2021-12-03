2c977d1ca451a8d75c501582b18d1951634c5e9f
Vicenthresh <vsolis199@gmail.com>
2021-12-02 22:12:54 -0300

    time measurement

--- binomial_main.c
+++ binomial_main.c
@@ -1,8 +1,11 @@
 #include "binomial.h"
+#include <time.h>
 
 int main(/*int argc, char ** argv*/) {
 	while (1) {
 		binomial_fn fn;
+		clock_t start, end;
+     	double cpu_time_used;
 		printf("Choose a binomial implementation.\n"
 		       "1. Slow\n"
 		       "2. Fast\n"
@@ -13,8 +16,10 @@ int main(/*int argc, char ** argv*/) {
 			break;
 		}
 		if (strat == 1) {
+			start = clock();
 			fn = get_binomial_slow();
 		} else if (strat == 2) {
+			start = clock();
 			fn = get_binomial_fast();
 		} else {
 			printf("Unknown strategy.\n");
@@ -31,8 +36,11 @@ int main(/*int argc, char ** argv*/) {
 			printf("Bye.\n");
 			break;
 		}
+		end = clock();
+     	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 		printf("Result: ");
 		printf("%ld\n\n", fn(n, k));
+		printf("Time: %f\n\n",cpu_time_used);
 	}
 	return 0;
 }
