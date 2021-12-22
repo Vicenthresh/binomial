6cafc0d01c05435f6250710746d805b135944b40
Vicenthresh <vsolis199@gmail.com>
2021-12-22 18:30:10 -0300

    dec to bin 2nd commit

--- dec_to_bin.c
+++ dec_to_bin.c
@@ -10,25 +10,38 @@ typedef struct large_number //estructura de datos utilizada
 } lnumber;
 
 lnumber LargeDivByTwo(lnumber number);
+void PrintNumber(lnumber number);
+char isNumber(char c);
 
 int main(){
 
     unsigned int len1 = 0, len2 = 0;
-    lnumber d, b;
+    lnumber d, bin;
+    int i=0;
 
-    char a[] = {"100"};
+    char a[] = {"42"};
     len1 = strlen(a);
     d.n = a;
     d.array_length = len1;
     d.current_array_length = len1;
 
     len2 = ceil(len1*(log(10)/log(2))); //max number of bits a len1-digit number can have
+    
+    bin.n = calloc(len2,sizeof(char)); // allocate memory
+    bin.array_length = len2;
+    bin.n[bin.array_length-1] = '0';
+    bin.current_array_length = 1;
 
-    printf("%s \n\n", d.n);
+    PrintNumber(d);
+    
+    while ( d.array_length > 0 ){
+        bin.n[i] = (char)(d.n[d.array_length-1] - '0') % 2 ;
+        d = LargeDivByTwo(d);
+        i++;
+    }
 
-    d = LargeDivByTwo(d);
+    PrintNumber(bin);
 
-    printf("%s \n\n", d.n);
 
     return 0;
 }
@@ -58,4 +71,25 @@ lnumber LargeDivByTwo(lnumber number){
     // falta caso cuando el divisor es mas grande q number
 
     return number;
+}
+
+void PrintNumber(lnumber number) {
+
+    unsigned int i, size = 0;
+    
+    //printf("\nNumber: ");
+
+    for (i = 0; i < number.array_length; i++)
+        if (isNumber(number.n[i]) == '1'){
+            printf("%c", number.n[i]);
+            //size = size + 1;
+        }
+    
+    //printf("\nArray_length: %d \n\n", size);
+}
+
+char isNumber(char c){
+    if (c >= '0' && c <= '9')
+        return '1';
+    return '0';
 }
